27046932ff4de099ff2f1d4261c3ae49e3f68d28
August Schleich <61630956+A4Atom@users.noreply.github.com>
2022-05-25 14:41:17 -0700

    Exented base.html to home, begin home header and product layout

--- razpi/core/views.py
+++ razpi/core/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def frontpage(request):
     return render(request, 'core/base.html')
+
+
+def home(request):
+    return render(request,"core/home.html")
--- razpi/razpi/urls.py
+++ razpi/razpi/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from core.views import frontpage
+from core.views import frontpage,home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',frontpage,name="frontpage"),
+    path('home',home, name="home")
 ]
