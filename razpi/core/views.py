779ac5856ed10b441c497605d0b5c08228b65e14
August Schleich <61630956+A4Atom@users.noreply.github.com>
2022-05-27 10:57:06 -0700

    Created product app, created Models: Category, product and displayed product on home

--- razpi/core/views.py
+++ razpi/core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from product.models import Product
 def frontpage(request):
     return render(request, 'core/base.html')
 
 
 def home(request):
-    return render(request,"core/home.html")
+    products = Product.objects.all()[0:8]
+    return render(request,"core/home.html", {"products": products})
