49d342e79de9b18d804ae5fa3e1d633bf56e5415
August Schleich <61630956+A4Atom@users.noreply.github.com>
2022-06-06 14:03:02 -0700

    added product templates: product view/html set static placeholders

--- razpi/razpi/urls.py
+++ razpi/razpi/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path
 from core.views import frontpage,home,shop,about
+from product.views import product
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('home/',home, name="home"),
     path('shop/',shop,name="shop"),
     path('about/',about,name="about"),
+    path('product/', product, name="product")
 ]
