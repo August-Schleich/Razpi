5bdf765e7284b9e810dbda838b2101dda94faedc
August Schleich <61630956+A4Atom@users.noreply.github.com>
2022-06-16 15:01:46 -0700

    Added menu_cart and add_to_cart attribute replaced base button with reusable button

--- razpi/razpi/urls.py
+++ razpi/razpi/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from core.views import frontpage,home,shop,about
 from product.views import product
+from cart.views import add_to_cart
 from django.views.generic.base import RedirectView
 from django.contrib.staticfiles.storage import staticfiles_storage
 
@@ -13,6 +14,7 @@ urlpatterns = [
     path('shop/',shop,name="shop"),
     path('about/',about,name="about"),
     path('shop/<slug:slug>/', product, name="product"),
+    path('add_to_cart/<int:product_id>/',add_to_cart, name="add_to_cart"),
     path(
         "razpi_icon.ico",
         RedirectView.as_view(url=staticfiles_storage.url("razpi_icon.ico")),
