d21e8fd17f1e5be8f246c74fd9641b24aa43c610
August Schleich <61630956+A4Atom@users.noreply.github.com>
2022-06-16 13:31:07 -0700

    fixed: favicon static render, set-up static folders: css, js and images

--- razpi/razpi/urls.py
+++ razpi/razpi/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path
 from core.views import frontpage,home,shop,about
 from product.views import product
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +12,9 @@ urlpatterns = [
     path('home/',home, name="home"),
     path('shop/',shop,name="shop"),
     path('about/',about,name="about"),
-    path('shop/<slug:slug>/', product, name="product")
+    path('shop/<slug:slug>/', product, name="product"),
+    path(
+        "razpi_icon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("razpi_icon.ico")),
+    ),
 ]
