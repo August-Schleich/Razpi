1fd787bea0213e74c9a93c0f46f71cb478a6de89
August Schleich <61630956+A4Atom@users.noreply.github.com>
2022-08-19 10:23:30 -0700

    Fixed: checkout auth redirect(possible change for un auth checkout case

--- razpi/cart/views.py
+++ razpi/cart/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cart.cart import Cart
 from django.contrib.auth.decorators import login_required
 # Create your views here.
-
+from django.contrib.auth import login
+from django.contrib.auth import views
 
 def add_to_cart(request, product_id):
     cart = Cart(request)
@@ -16,3 +17,12 @@ def cart(request):
 @login_required
 def checkout(request):
     return render(request,'cart/checkout.html')
+
+# def login_checkout(request):
+#       form = views.LoginView(request.POST)
+#       if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+            
+#             return redirect('cart/checkout')
+#       return render (request, 'cart/login_checkout.html')
--- razpi/product/views.py
+++ razpi/product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.contrib import messages
 
 def product(request, slug):
     product = get_object_or_404(Product, slug=slug)
