f22ccd8c824d26da273943b9dd5f03a79f64ce71
August Schleich <61630956+A4Atom@users.noreply.github.com>
2022-08-22 11:23:25 -0700

    Fixed item 0 increment, install/set up stripe payment, created session view and validation for checkout

--- razpi/cart/views.py
+++ razpi/cart/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render, redirect
 from cart.cart import Cart
 from django.contrib.auth.decorators import login_required
@@ -15,35 +16,42 @@ def add_to_cart(request, product_id):
 def cart(request):
     return render(request,'cart/cart.html')
 
-def update_cart(request, product_id,action):
+def update_cart(request, product_id, action):
     cart = Cart(request)
-    
+
     if action == 'increment':
         cart.add(product_id, 1, True)
     else:
         cart.add(product_id, -1, True)
-        
-    product = Product.objects.get(id=product_id)
-    quantity = cart.get_item(product_id)['quantity']
     
-    item = {
-        'product': {
-            'id': product_id,
-            'name': product.name,
-            'image': product.image,
-            'get_thumbnail': product.get_thumbnail(),
-            'price': product.price,
-        },
-        'total_price': (quantity * product.price) / 100,
-        'quantity': quantity,
-    }
+    product = Product.objects.get(pk=product_id)
+    quantity = cart.get_item(product_id)
+    
+    if quantity:
+        quantity = quantity['quantity']
+
+        item = {
+            'product': {
+                'id': product.id,
+                'name': product.name,
+                'image': product.image,
+                'get_thumbnail': product.get_thumbnail(),
+                'price': product.price,
+            },
+            'total_price': (quantity * product.price) / 100,
+            'quantity': quantity,
+        }
+    else:
+        item = None
+        
     response = render(request, 'cart/partials/cart_item.html', {'item': item})
     response['HX-Trigger'] = 'update-menu-cart'
      
     return response
 @login_required
 def checkout(request):
-    return render(request,'cart/checkout.html')
+    pub_key = settings.STRIPE_API_KEY_PUBLISHABLE
+    return render(request,'cart/checkout.html', {'pub_key':pub_key})
 
 def hx_menu_cart(request):
     return render(request,'cart/menu_cart.html')
